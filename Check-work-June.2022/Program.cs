fbfbb5d56602c22c44e0cc5dbcdb0833b873476b
NickAllenov <AllenovNS@gmail.com>
2022-06-18 08:05:23 +0300

    Oh! That looks much better

--- Check-work-June.2022/Program.cs
+++ Check-work-June.2022/Program.cs
@@ -1,10 +1,10 @@
-﻿string[] CreateInitialArray(string[] array, int size)
+﻿string[] CreateInitialArray(string[] array, int size, int min, int max)
 {
     string[] initialArray = new string[size];
 
     for (int i = 0; i < size; i++)              // "i++" means the same as "i += 1" or "i = i + 1"
     {
-        initialArray[i] = array[new Random().Next(0, initialArray.Length + 1)];
+        initialArray[i] = array[new Random().Next(min, max)];
     }
 
     return initialArray;
@@ -39,12 +39,24 @@ string[] NewArray(string[] array)
         return new string[0];
 }
 
-void PrintArray(string[] array)
+void PrintArrayVar1(string[] array)
 {
-    Console.Write("[");
+    Console.Write("[ ");
     for (int i = 0; i < array.Length; i++)          // "i++" means the same as "i += 1" or "i = i + 1"
-        Console.Write(array[i] + ", ");
+        Console.Write(array[i] + " ");
     Console.WriteLine("]");
+}
+
+string PrintArrayVar2(string[] array)
+{
+    string result = string.Empty;
+
+    result += "[ ";
+    for (int i = 0; i < array.Length; i++)
+        result += $"{array[i]} ";
+    result += "]";
+
+    return result;
 }
 
 string[] startStrings = {"IT", "is", "the", "future!", "AI", "BI", "Cyber", "Security", "Drones", "Space", 
@@ -53,6 +65,13 @@ string[] startStrings = {"IT", "is", "the", "future!", "AI", "BI", "Cyber", "Sec
 Console.WriteLine("Select the length of the initial array in range from 1 to 20: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
-PrintArray(CreateInitialArray(startStrings, size));
-Console.WriteLine();
-PrintArray(NewArray(CreateInitialArray(startStrings, size)));
+if (size > 0 && size <= startStrings.Length)
+{
+    string[] initialArr = CreateInitialArray(startStrings, size, 0, startStrings.Length);
+    PrintArrayVar1(initialArr);
+    string[] newArr = NewArray(initialArr);
+    PrintArrayVar1(newArr);
+    Console.WriteLine(PrintArrayVar2(newArr));
+}
+else
+    Console.WriteLine("Invalid value entered! Please enter a number between 1 and " + startStrings.Length);
